702e42e65b9044a72cf923295b938c801f2f6b98
Chris Zhu <christopher.zhu.9@gmail.com>
2017-01-28 15:57:49 -0500

    Start LoginActivity UI

--- Android/app/src/main/java/deltahacks3/agora/LoginActivity.java
+++ Android/app/src/main/java/deltahacks3/agora/LoginActivity.java
@@ -1,8 +1,27 @@
 package deltahacks3.agora;
 
-/**
- * Created by chris on 1/28/17.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Toast;
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton fbLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.fbLoginBtn = (AppCompatButton) findViewById(R.id.fb_login_btn);
+        this.fbLoginBtn.setClickable(true);
+        this.fbLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked FB button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
