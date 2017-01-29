162d9c7b4f53f27e95761f01fa3fa193aab30705
Chris Zhu <christopher.zhu.9@gmail.com>
2017-01-29 08:34:48 -0500

    Finish rough Facebook login implementationC

--- Android/app/src/main/java/deltahacks3/agora/LoginActivity.java
+++ Android/app/src/main/java/deltahacks3/agora/LoginActivity.java
@@ -2,103 +2,100 @@ package deltahacks3.agora;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.AccessTokenTracker;
+import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
-    CallbackManager callbackManager;
-    LoginButton fbLoginBtn;
-    AccessTokenTracker accessTokenTracker;
+
+    private String username;
+    private Uri profileUri;
+
+    LoginButton loginButton;
+//    FrameLayout fbFragContainer;
+
     AccessToken accessToken;
+    private CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
     ProfileTracker profileTracker;
-    Profile profile;
-    Arrays Arrays;
-
-    @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-        LoginButton authButton = (LoginButton)findViewById(R.id.fb_login_button);
-        authButton.setFragment(this);
-        authButton.setReadPermissions(Arrays.asList("public_profile"));
-
-        return view;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        callbackManager = CallbackManager.Factory.create();
+
+        /*FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);*/
         setContentView(R.layout.activity_login);
-        accessTokenTracker = new AccessTokenTracker() {
+
+        /*this.fbFragContainer = (FrameLayout) findViewById(R.id.fb_frag_container);
+        FacebookFragment fbFrag = new FacebookFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fb_frag_container, fbFrag, "fb_fragment").commit();*/
+
+        /*accessTokenTracker = new AccessTokenTracker() {
             @Override
-            protected void onCurrentAccessTokenChanged(
-                    AccessToken oldAccessToken,
-                    AccessToken currentAccessToken) {
-                // Set the access token using
-                // currentAccessToken when it's loaded or set.
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                       AccessToken currentAccessToken) {
+                accessToken = currentAccessToken;
             }
-        };
-        // If the access token is available already assign it.
-        accessToken = AccessToken.getCurrentAccessToken();
+        };*/
 
         profileTracker = new ProfileTracker() {
             @Override
-            protected void onCurrentProfileChanged(
-                    Profile oldProfile,
-                    Profile currentProfile) {
-                // App code
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                username = currentProfile.getFirstName();
+//              TODO:  profileUri = currentProfile.getProfilePictureUri(..);
             }
         };
 
-        // If the access token is available already assign it.
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
-//        getActionBar().hide(); // getActionBar() returns null
+        if (this.callbackManager == null) {
+            this.callbackManager = CallbackManager.Factory.create();
+        }
 
-        this.fbLoginBtn = (LoginButton) findViewById(R.id.fb_login_button);
-        fbLoginBtn.setReadPermissions("email");
-        this.fbLoginBtn.setClickable(true);
-        fbLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        loginButton.setReadPermissions("public_profile");
+        loginButton.registerCallback(this.callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                Toast.makeText(getApplicationContext(), "Clicked FB button", Toast.LENGTH_SHORT).show();
-
-                // Start the MapsActivity
-                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-                startActivity(intent);
-
+                accessToken = loginResult.getAccessToken();
+                Toast.makeText(LoginActivity.this, "FB Login success", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onCancel() {
                 // App code
+                Toast.makeText(LoginActivity.this, "FB Login cancelled", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException exception) {
                 // App code
+                Toast.makeText(LoginActivity.this, "FB Login error", Toast.LENGTH_SHORT).show();
             }
+        });
 
-    });
-
+        accessToken = AccessToken.getCurrentAccessToken();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
